3c8ba117ac5b0e6b33f6f13c54c8d8590f398609
Tomy Prasetyo <--global>
2021-05-11 00:38:22 +0700

    penambahan menu data iklan

---
+++ application/controllers/admin/Iklan.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Iklan extends MY_Controller {
+	function __construct(){
+        parent::__construct();
+		$this->load->model('m_login');
+		$this->load->model('m_akun');
+		$this->load->model('m_crud');
+		$this->load->model('m_iklan');
+
+		if($this->is_not_login()){
+			redirect(base_url('login'));
+		}
+
+        if($this->is_not_admin()){
+			show_404();
+		}
+    }
+
+	public function index()
+	{
+		$data["title"]		= "Iklan";
+		$data['iklan']		= $this->m_iklan->tampil_data()->result();
+		$this->load->view('admin/head',$data);
+		$this->load->view('admin/menu',$data);
+		$this->load->view('admin/iklan',$data);
+		$this->load->view('admin/footer',$data);
+	}
+
+	
+
+	
+}
--- application/models/M_iklan.php
+++ application/models/M_iklan.php
@@ -2,12 +2,17 @@
 class M_iklan extends CI_Model{
 
   function tampil_data(){
-    $query=$this->db->query("SELECT * FROM tb_iklan");
+    $query=$this->db->query("SELECT * FROM tbl_iklan");
+    return $query;
+  }
+
+  function cari_iklan($a){
+    $query=$this->db->query("SELECT * FROM tbl_iklan WHERE id_iklan LIKE '%$a%' LIMIT 1")->result();
     return $query;
   }
 
   function edit_data($a){
-    $query=$this->db->query("SELECT * FROM tb_iklan WHERE banner_id = '$a'");
+    $query=$this->db->query("SELECT * FROM tbl_iklan WHERE id_iklan = '$a'");
     return $query;
   }
 }
